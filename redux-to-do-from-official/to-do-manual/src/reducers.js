e9edebf3fcabeba6c2d90d16752d3ff62afed448
Anton Smirnov <antosha-85@yandex.ru>
2020-06-28 00:24:03 -0600

    Created reducers

---
+++ redux-to-do-from-official/to-do-manual/src/reducers.js
@@ -0,0 +1,46 @@
+import {
+  SET_VISIBILITY_FILTER,
+  ADD_TODO,
+  TOGGLE_TODO,
+  VisibilityFilters,
+} from "./actions";
+
+const initialState = {
+  visibilityFilter: visibilityFilter.SHOW_ALL,
+  todos: [],
+};
+
+function todoApp(state = initialState, action) {
+  //   if (typeof state === "undefined") {
+  //     return initialState;
+  //   }
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter,
+      });
+      case ADD_TODO: 
+      return Object.assign({}, state, {
+          todos: [
+              ...state.todos,
+              {
+                  text: action.text,
+                  completed: false
+              }
+          ]
+      })
+      case TOGGLE_TODO:
+          return Object.assign({}, state, {
+              todos: state.todos.map((todo, index)=> {
+                  if(index === action.index) {
+                      return Object.assign({}, todo, {
+                          completed: !todo.completed
+                      })
+                  }
+                  return todo
+              })
+          })
+    default:
+      return state;
+  }
+}
