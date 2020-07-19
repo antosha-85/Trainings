770e3e74f206b2373277b8e32c83b1991c9605bb
Anton Smirnov <antosha-85@yandex.ru>
2020-07-18 23:36:40 -0600

    Configured webpack for css

--- Webpack_Max/webpack.config.js
+++ Webpack_Max/webpack.config.js
@@ -1,21 +1,47 @@
-const path = require('path')
+const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-    mode: 'development',
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
-        publicPath: ''
-    },
-    devtool: 'cheap-module-eval-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  devtool: "cheap-module-eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+              },
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [autoprefixer()],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
