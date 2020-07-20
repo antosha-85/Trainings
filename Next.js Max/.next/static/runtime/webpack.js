9229984fdbbe8e531da2ffa6ccdf5492288d44d8
Anton Smirnov <antosha-85@yandex.ru>
2020-07-19 23:22:49 -0600

    Added a User component and passed a couple of props

--- Next.js Max/.next/static/runtime/webpack.js
+++ Next.js Max/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5c0da57393d42b05e94";
+/******/ 	var hotCurrentHash = "d645c99196ad40b32e95";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Next.js Max/pages/index.js
+++ Next.js Max/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from 'next/link'
 import Router from 'next/router'
+
+
 const indexPage = () => (
   <div>
     <h1>The Main Page</h1>
