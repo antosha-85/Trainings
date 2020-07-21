0e7303e3356ab44e35d95d3a04db34370acf0ac4
Anton Smirnov <antosha-85@yandex.ru>
2020-07-21 15:06:26 -0600

    Wrapped our div in a Transition and set in prop and timeout

--- React Animations/src/App.js
+++ React Animations/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -22,24 +23,24 @@ class App extends Component {
       <div className="App">
         <h1>React Animations</h1>
         <button
-        className="Button"
+          className="Button"
           onClick={() =>
             this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
           }
         >
           Toggle
         </button>
-        <br/>
-        {this.state.showBlock ? (
+        <br />
+        <Transition in={this.state.showBlock} timeout={300}>
           <div
             style={{
               backgroundColor: "red",
               width: 100,
               height: 100,
-              margin: 'auto'
+              margin: "auto",
             }}
           ></div>
-        ) : null}
+        </Transition>
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         ) : null}
