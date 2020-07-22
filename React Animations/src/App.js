0a2f0cfc5aba1b6cf10bd110150177ae816b2013
Anton Smirnov <antosha-85@yandex.ru>
2020-07-21 22:35:27 -0600

    Added transition component and used it for animation along with a transition css property

--- React Animations/src/App.js
+++ React Animations/src/App.js
@@ -31,15 +31,24 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        <Transition in={this.state.showBlock} timeout={300}>
-          <div
-            style={{
-              backgroundColor: "red",
-              width: 100,
-              height: 100,
-              margin: "auto",
-            }}
-          ></div>
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
         </Transition>
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
