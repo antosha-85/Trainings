f59c57952c85c3e6db405ab899588c7184e924d2
Anton Smirnov <antosha-85@yandex.ru>
2020-07-30 13:43:43 -0600

    Added a new state of loading for initial render, deleting and adding ingredients. If loading is true we see a spinner

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetch('https://react-hooks-update-e9a47.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
+    setIsLoading(true);
+    fetch("https://react-hooks-update-e9a47.firebaseio.com/ingredients.json")
+      .then((response) => {
+        response.json();
+        setIsLoading(false);
+      })
+      .then((responseData) => {
         const loadedIngredients = [];
         for (const key in responseData) {
           loadedIngredients.push({
             id: key,
             title: responseData[key].title,
-            amount: responseData[key].amount
+            amount: responseData[key].amount,
           });
         }
         setUserIngredients(loadedIngredients);
@@ -24,45 +29,53 @@ const Ingredients = () => {
   }, []);
 
   useEffect(() => {
-    console.log('RENDERING INGREDIENTS', userIngredients);
+    console.log("RENDERING INGREDIENTS", userIngredients);
   }, [userIngredients]);
 
-  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
   }, []);
 
-  const addIngredientHandler = ingredient => {
-    fetch('https://react-hooks-update-e9a47.firebaseio.com/ingredients.json', {
-      method: 'POST',
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true)
+    fetch("https://react-hooks-update-e9a47.firebaseio.com/ingredients.json", {
+      method: "POST",
       body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     })
-      .then(response => {
+      .then((response) => {
+        setIsLoading(false)
         return response.json();
       })
-      .then(responseData => {
-        setUserIngredients(prevIngredients => [
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
           ...prevIngredients,
-          { id: responseData.name, ...ingredient }
+          { id: responseData.name, ...ingredient },
         ]);
       });
   };
 
-  const removeIngredientHandler = ingredientId => {
-    fetch(`https://react-hooks-update-e9a47.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    })
-    .then(response => {
-
-      setUserIngredients(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+  const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
+    fetch(
+      `https://react-hooks-update-e9a47.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then((response) => {
+      setIsLoading(false);
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
       );
-    })
+    });
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
