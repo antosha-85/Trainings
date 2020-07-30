80bcbdfee5f2730d7d00edfd257b0a4ce3925bcd
Anton Smirnov <antosha-85@yandex.ru>
2020-07-29 23:24:52 -0600

    Implemented a query search and made a filter working if we type the exact name of our ingredient

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,58 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import IngredientForm from "./IngredientForm";
-import IngredientList from "./IngredientList";
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
 
-import Search from "./Search";
-
-function Ingredients() {
+const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   useEffect(() => {
-    fetch("https://react-hooks-update-e9a47.firebaseio.com/ingredients.json")
-      .then((response) => response.json())
-      .then((responseData) => {
+    fetch('https://react-hooks-update-e9a47.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
         const loadedIngredients = [];
         for (const key in responseData) {
           loadedIngredients.push({
             id: key,
             title: responseData[key].title,
-            amount: responseData[key].amount,
+            amount: responseData[key].amount
           });
         }
         setUserIngredients(loadedIngredients);
-        console.log("Ingredients -> loadedIngredients", loadedIngredients);
       });
   }, []);
+
   useEffect(() => {
-    console.log("Rendering ingredients", userIngredients);
+    console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
-  const addIngredientHandler = (ingredient) => {
-    fetch("https://react-hooks-update-e9a47.firebaseio.com/ingredients.json", {
-      method: "POST",
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-update-e9a47.firebaseio.com/ingredients.json', {
+      method: 'POST',
       body: JSON.stringify(ingredient),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      return response.json().then((responseData) => {
-        setUserIngredients((prevIngredients) => [
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
           ...prevIngredients,
-          { id: responseData.name, ...ingredient },
+          { id: responseData.name, ...ingredient }
         ]);
       });
-    });
   };
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
