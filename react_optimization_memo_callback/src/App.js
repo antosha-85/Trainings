f93bf744fd9ca935e9529a0e5b59822169405b3d
Anton Smirnov <antosha-85@yandex.ru>
2020-08-08 23:09:21 -0600

    Applied useCallback hook for a fetchData function in App.js

--- react_optimization_memo_callback/src/App.js
+++ react_optimization_memo_callback/src/App.js
@@ -1,12 +1,23 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect, useCallback } from "react";
 import "./App.css";
 import ChildComponent from "./components/ChildComponent";
 // const array = ["One", "Two", "Three"] alternative of useMemo especially if our array doesn't depend on anything
 function App() {
   const [count, setCount] = useState(0);
   const array = useMemo(() => {
-    return ["One", "Two", "Three"];
+    const array = ["One", "Two", "Three"];
+    return array;
   }, []);
+
+  const fetchData = useCallback(type => {
+     fetch(`https://jsonplaceholder.typicode.com/${type}/1`)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }, [])
+  useEffect(() => {
+   fetchData('todos')
+  }, [fetchData]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +25,7 @@ function App() {
         <button type="button" onClick={() => setCount(count + 1)}>
           ADD
         </button>
-        <ChildComponent title="This is a title!" array={array} />
+        <ChildComponent title="This is a title!" array={array} fetch={fetchData}/>
       </header>
     </div>
   );
--- react_optimization_memo_callback/src/components/ChildComponent.js
+++ react_optimization_memo_callback/src/components/ChildComponent.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ChildComponent = React.memo((props) => {
   console.log("[ChildComponent] rendering!");
+  const splitArray = props.array.join(" ");
+  const { fetch } = props;
+  useEffect(() => {
+    fetch("users");
+  }, [fetch]);
   return (
     <div>
       <h2>{props.title}</h2>
       <p>Child Component!</p>
+      {splitArray}
     </div>
   );
 });
