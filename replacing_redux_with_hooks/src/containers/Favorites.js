6f2de6db35fff3c217a4488af66db1b13a819e20
Anton Smirnov <antosha-85@yandex.ru>
2020-08-09 23:52:10 -0600

    Updated Favorites.js and ProductItem.js with useContext hook

--- replacing_redux_with_hooks/src/containers/Favorites.js
+++ replacing_redux_with_hooks/src/containers/Favorites.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
+import React, {useContext} from 'react';
+// import { useSelector } from 'react-redux';
+import {ProductsContext} from '../context/products_context'
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const products = useContext(ProductsContext).products1.filter(
+    p => p.isFavorite
+  )
+  // const favoriteProducts = useSelector(state =>
+  //   state.shop.products.filter(p => p.isFavorite)
+  // );
   let content = <p className="placeholder">Got no favorites yet!</p>;
-  if (favoriteProducts.length > 0) {
+  if (products.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {products.map(prod => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
--- replacing_redux_with_hooks/src/context/products_context.js
+++ replacing_redux_with_hooks/src/context/products_context.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
-export const ProductsContext = React.createContext({ products1: [] });
+export const ProductsContext = React.createContext({
+  products1: [],
+  toggleFav: (id) => {},
+});
 
 export default (props) => {
   const [productsList, setProductsList] = useState([
@@ -29,8 +32,23 @@ export default (props) => {
       isFavorite: false,
     },
   ]);
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
   return (
-    <ProductsContext.Provider value={{ products1: productsList }}>
+    <ProductsContext.Provider
+      value={{ products1: productsList, toggle: toggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
