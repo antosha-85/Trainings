8133898cdf0445bc47f9e6f2aba7aaeee2457c1d
Anton Smirnov <antosha-85@yandex.ru>
2020-08-10 22:24:36 -0600

    Created dispatch function that takes action identifier and updates global state. Then we loop through listeners array and call listener function with the updated global state, and finally we return global state and dispatch

--- replacing_redux_with_hooks/src/hooks_store/store.js
+++ replacing_redux_with_hooks/src/hooks_store/store.js
@@ -4,12 +4,21 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+    for (const listener of listeners) {
+        listener(globalState)
+    }
+  };
   useEffect(() => {
     listeners.push(setState);
-    return ()=> {
-        listeners = listeners.filter(listener => listener !== setState
-    }
+    return () =>
+      (listeners = listeners.filter((listener) => listener !== setState));
   }, [setState]);
+  return [globalState, dispatch]
 };
+
