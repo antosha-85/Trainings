f93168c8600765bec11b8c62c805294b63fc8ac7
Anton Smirnov <antosha-85@yandex.ru>
2020-12-27 15:59:44 -0500

    Cleaned symfony

---
+++ react_tr/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+import Todos from './Components/Todos'
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with Oksana',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Roll out spools in the bush',
+        completed: false
+      },
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react_tr/src/Components/Todos.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+
+export default class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem todo={todo} key={todo.id} />
+    ));
+  }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+};
