bcdaaec42a070f0163fb0d5e3b2516893f7cd5c7
Anton Smirnov <antosha-85@yandex.ru>
2020-12-28 21:18:16 -0700

    Added some styling and added a checkbox before the todo

--- react_tr/src/Components/TodoItem.js
+++ react_tr/src/Components/TodoItem.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.todo.title}</p>
-            </div>
-        )
-    }
+  getStyle = () => {
+    return {
+      background: "#f4ff4f4",
+      padding: "10px",
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+  render() {
+    return (
+      <div style={this.getStyle()}>
+        <p>
+            <input type="checkbox"/> {' '}
+            {this.props.todo.title}</p>
+      </div>
+    );
+  }
 }
 
+// const itemStyle = {
+//     backgroundColor: '#f4f4f4'
+// }
 
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired,
-  };
+  todo: PropTypes.object.isRequired,
+};
 
-export default TodoItem
+export default TodoItem;
