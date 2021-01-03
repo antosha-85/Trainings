5a510404cc1b9e17cbd92c2bc8cd6426ca15baf3
antosha-85 <antosha-85@yandex.ru>
2021-01-02 23:47:09 -0700

    Added window width as a state and on resize I update that value

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [resourceType, setResourceType] = useState("posts");
-  const [items, setItems] = useState([])
-  console.log("render!");
+  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return window.removeEventListener("resize", handleResize);
+  }, []);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
       .then((response) => response.json())
@@ -13,14 +20,15 @@ function App() {
   }, [resourceType]);
   return (
     <>
+      <h1>Window width: {windowWidth}</h1>
       <div>
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
         <button onClick={() => setResourceType("comments")}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
-      {items.map(item => (
-        <pre>{JSON.stringify(item)}</pre>
+      {items.map((item) => (
+        <pre key={item.title}>{JSON.stringify(item)}</pre>
       ))}
     </>
   );
