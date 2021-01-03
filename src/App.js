458084972d9eb7f5a77a3e98b36b2bca915573ab
antosha-85 <antosha-85@yandex.ru>
2021-01-02 22:44:07 -0700

    Initialized a new project and created and used useEffect hook to make a fetch request

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  console.log("render!");
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, [resourceType]);
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  );
+}
+
+export default App;
