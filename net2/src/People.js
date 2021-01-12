c09dfa2ae5d942199360f79707b9370b0858a6f6
antosha-85 <antosha-85@yandex.ru>
2021-01-11 19:58:49 -0700

    Converted People component from class to function

--- net2/src/People.js
+++ net2/src/People.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class People extends Component {
-  render() {
-    const {people} = this.props;
-    return people.map((person) => {
-        const {name, age, gender, id} = person;
-      return (
-        <div className="anton" key={id}>
-          <div>Name: {name}</div>
-          <div>Age: {age}</div>
-          <div>I'm {gender}</div>
-          <div>Welcome, friends!</div>
-        </div>
-      );
-    });
-  }
-}
+const People = (props) => {
+  const { people } = props;
+  return people.map((person) => {
+    const { name, age, gender, id } = person;
+    return (
+      <div className="anton" key={id}>
+        <div>Name: {name}</div>
+        <div>Age: {age}</div>
+        <div>I'm {gender}</div>
+        <div>Welcome, friends!</div>
+      </div>
+    );
+  });
+}; 
+
+export default People;
