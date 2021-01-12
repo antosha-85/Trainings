3947d40a36afea1b904c6eb13b6784d24923fc2f
antosha-85 <antosha-85@yandex.ru>
2021-01-11 20:09:06 -0700

    Conditionally rendered my content based on age

--- net2/src/People.js
+++ net2/src/People.js
@@ -4,15 +4,15 @@ const People = (props) => {
   const { people } = props;
   return people.map((person) => {
     const { name, age, gender, id } = person;
-    return (
+    return age >= 35 ? (
       <div className="anton" key={id}>
         <div>Name: {name}</div>
         <div>Age: {age}</div>
         <div>I'm {gender}</div>
         <div>Welcome, friends!</div>
       </div>
-    );
+    ) : null;
   });
-}; 
+};
 
 export default People;
