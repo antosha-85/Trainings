ef31c969a4072551a3c6fdbce9c43c7f9ebc0cfd
antosha-85 <antosha-85@yandex.ru>
2021-01-16 22:31:12 -0700

    Installed axios and then did a get request inside of the ComponentDidMount lifecycle to setup our state

--- net4/src/components/Home.js
+++ net4/src/components/Home.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae atque
-        labore iure eaque tenetur! Totam quaerat, asperiores dignissimos enim
-        nihil natus quod! Eligendi unde voluptatibus quia sequi ab corrupti
-        harum?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      this.setState({ posts: res.data.slice(0, 10) });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = !posts.length ? (
+      <div className="center">No posts yet!</div>
+    ) : (
+      posts.map((post) => (
+        <div className="post card" key={post.id}>
+          <div className="card-content">
+            <span className="card-title">{post.title}</span>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      ))
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
