de4b239a45676f267a7ae90027763a3a4ae8d6b9
antosha-85 <antosha-85@yandex.ru>
2021-01-19 22:08:24 -0700

    Mapped our state to props and then connected it to the central Redux store

--- net4/src/components/Home.js
+++ net4/src/components/Home.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import Free from "../Free.png";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: [],
-  };
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      this.setState({ posts: res.data.slice(0, 10) });
-    });
-  }
+  // state = {
+  //   posts: [],
+  // };
+  // componentDidMount() {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+  //     this.setState({ posts: res.data.slice(0, 10) });
+  //   });
+  // }
   render() {
-    const { posts } = this.state;
+    console.log(this.props);
+    const { posts } = this.props;
     const postList = !posts.length ? (
       <div className="center">No posts yet!</div>
     ) : (
       posts.map((post) => (
         <div className="post card" key={post.id}>
-          <img src={Free} alt="Free"/>
+          <img src={Free} alt="Free" />
           <div className="card-content">
             <Link to={`/${post.id}`}>
               <span className="card-title red-text">{post.title}</span>
@@ -38,4 +40,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
--- net4/src/reducers/mainReducer.js
+++ net4/src/reducers/mainReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-  posts: [],
+  posts: [
+    {
+      id: "1",
+      title: "Lorem ipsum dolor sit amet",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eius ipsa natus. Optio, dicta harum?",
+    },
+    {
+      id: "2",
+      title: "Lorem ipsum dolor sit amet consectetur.",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eius ipsa natus. Optio, dicta harum?",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit.",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eius ipsa natus. Optio, dicta harum?",
+    },
+  ],
 };
 
 const mainReducer = (state = initState, action) => {
