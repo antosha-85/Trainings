f1f7e0a76012e9b2a2170b959217a154c596adbf
antosha-85 <antosha-85@yandex.ru>
2021-01-20 22:44:19 -0700

    Created an action for posts

--- net4/src/components/Post.js
+++ net4/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { deletePost } from '../actions/postActions'
 // import axios from "axios";
 
 const mapStateToProps = (state, ownProps) => {
@@ -11,7 +12,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deletePost: (id) => dispatch({ type: "DELETE_POST", id: id }),
+    deletePost: (id) => dispatch(deletePost(id)),
   };
 };
 export default connect(
