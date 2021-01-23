a9b65be058ed4a5879628330cd4a1f11fc9ca8ef
antosha-85 <antosha-85@yandex.ru>
2021-01-22 20:14:00 -0700

    Initialized a new project, created a structure and installed BrowserRouter

---
+++ net6/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Hi Anton</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
