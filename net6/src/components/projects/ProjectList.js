812554345015b2a190a73396b7bedbb1e5797fa4
antosha-85 <antosha-85@yandex.ru>
2021-01-26 21:27:17 -0700

    Created a new Project summary and listed it inside the ProjectList

--- net6/src/components/projects/ProjectList.js
+++ net6/src/components/projects/ProjectList.js
@@ -1,29 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Anton Smirnov</p>
-          <p className="grey-text">26-JAN-2021, 9PM</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title1</span>
-          <p>Posted by Anton Smirnov</p>
-          <p className="grey-text">27-JAN-2021, 9PM</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title2</span>
-          <p>Posted by Anton Smirnov</p>
-          <p className="grey-text">28-JAN-2021, 9PM</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ net6/src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Anton Smirnov</p>
+        <p className="grey-text">26-JAN-2021, 9PM</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
