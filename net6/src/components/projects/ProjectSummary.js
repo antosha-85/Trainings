5d23127f9b9e63a98d50c343243dddb97b7b6a69
antosha-85 <antosha-85@yandex.ru>
2021-01-30 20:57:58 -0700

    Added a dummy projects array, connected it to the central store and passed all the way to ProjectList and ProjectSummary

--- net6/src/components/projects/ProjectSummary.js
+++ net6/src/components/projects/ProjectSummary.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project: { title, content } }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{title}</span>
+        <p>{content}</p>
         <p>Posted by Anton Smirnov</p>
         <p className="grey-text">26-JAN-2021, 9PM</p>
       </div>
--- net6/src/store/reducers/projectReducer.js
+++ net6/src/store/reducers/projectReducer.js
@@ -1,4 +1,25 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "hi guys 1",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quaerat.1",
+    },
+    {
+      id: "2",
+      title: "hi guys 2",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quaerat.2",
+    },
+    {
+      id: "3",
+      title: "hi guys 3",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quaerat.3",
+    },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
