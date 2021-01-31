75899667617e35ae3d873c073799c9e1749e7710
antosha-85 <antosha-85@yandex.ru>
2021-01-31 00:03:07 -0700

    Created a firebase config file and also installed firebase

---
+++ net6/src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//Initialize Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfl_292ivH3E3DVykqwAED7rMD7-b9yKs",
+  authDomain: "anton-plan.firebaseapp.com",
+  projectId: "anton-plan",
+  storageBucket: "anton-plan.appspot.com",
+  messagingSenderId: "500276286187",
+  appId: "1:500276286187:web:71fb4ccdd8cf461d0564ae",
+  measurementId: "G-NHPL9H3Q7W",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+firebase.analytics();
+
+export default firebase;
