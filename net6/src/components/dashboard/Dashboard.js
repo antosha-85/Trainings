92f0ab6d6405251e2ec5721f51bb8894ba432486
antosha-85 <antosha-85@yandex.ru>
2021-02-05 23:12:59 -0700

    Instead of dummy data using firestoreReducer to render projects that are synced with firestore

--- net6/src/components/dashboard/Dashboard.js
+++ net6/src/components/dashboard/Dashboard.js
@@ -24,8 +24,9 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log("state", state);
   return {
-    projects: state.project.projects,
+    projects: state.firestore.ordered.Projects,
   };
 };
 export default compose(
