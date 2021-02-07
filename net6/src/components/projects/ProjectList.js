9caca7dfbc7c06172d0d89a7babcdfffa66ed03d
antosha-85 <antosha-85@yandex.ru>
2021-02-06 21:30:43 -0700

    Wrapped each project into Link and directed to /project/project.id

--- net6/src/components/projects/ProjectList.js
+++ net6/src/components/projects/ProjectList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
 const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
       {projects &&
-        projects.map((project) => <ProjectSummary project={project} key={project.id} />)}
+        projects.map((project) => (
+          <Link to={`/project/${project.id}`}>
+            <ProjectSummary project={project} key={project.id} />
+          </Link>
+        ))}
     </div>
   );
 };
