8a482fdf12eb21f1311dfa2f75c03b13e47be863
antosha-85 <antosha-85@yandex.ru>
2021-02-08 23:08:18 -0700

    Guarded signin and signup when the user is logged in

--- net6/src/components/auth/SignIn.js
+++ net6/src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { signIn } from "../../store/actions/authActions";
+import { Redirect } from "react-router-dom";
 
 class SignIn extends Component {
   state = {
@@ -19,7 +20,10 @@ class SignIn extends Component {
 
   handle;
   render() {
-    const { authError } = this.props;
+    const { authError, auth } = this.props;
+    if (auth.uid) {
+      return <Redirect to="/" />;
+    }
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -47,6 +51,7 @@ class SignIn extends Component {
 const mapStateToProps = (state) => {
   return {
     authError: state.auth.authError,
+    auth: state.firebase.auth,
   };
 };
 const mapDispatchToProps = (dispatch) => {
