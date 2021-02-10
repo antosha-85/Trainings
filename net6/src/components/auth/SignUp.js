17c16d3a45bbe3aafb07478d4dad2c0ff1fb64bc
antosha-85 <antosha-85@yandex.ru>
2021-02-09 21:33:31 -0700

    on Sign up I use onSignUp method and in case of an error I render that error underneath the form

--- net6/src/components/auth/SignUp.js
+++ net6/src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { signUp } from "../../store/actions/authActions";
 
 class SignUp extends Component {
   state = {
@@ -11,7 +12,7 @@ class SignUp extends Component {
   };
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.onSignUp(this.state);
   };
   handleChange = (e) => {
     this.setState({
@@ -20,7 +21,7 @@ class SignUp extends Component {
   };
   handle;
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) {
       return <Redirect to="/" />;
     }
@@ -47,6 +48,9 @@ class SignUp extends Component {
           <div className="input-field">
             <button className="btn pink lighten-1 z-index-0">Sign up</button>
           </div>
+          <div className="red-text center">
+            {authError ? <p>{authError}</p> : null}
+          </div>
         </form>
       </div>
     );
@@ -56,7 +60,16 @@ class SignUp extends Component {
 const mapStateToProps = (state) => {
   return {
     auth: state.firebase.auth,
+    authError: state.auth.authError,
   };
 };
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSignUp: (newUser) => {
+      dispatch(signUp(newUser));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
