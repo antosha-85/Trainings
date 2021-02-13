8c3450ced748b86661ccd29ba3e606238399ca50
antosha-85 <antosha-85@yandex.ru>
2021-02-12 20:55:44 -0700

    Updated our hardcoded dates with the help of moment library for ProjectSummary and ProjectDetails components

--- net6/src/components/projects/ProjectSummary.js
+++ net6/src/components/projects/ProjectSummary.js
@@ -1,13 +1,18 @@
 import React from "react";
+import moment from "moment";
 
-const ProjectSummary = ({ project: { title, content, authorFirstName, authorLastName } }) => {
+const ProjectSummary = ({
+  project: { title, content, authorFirstName, authorLastName, createdAt },
+}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{title}</span>
         <p>{content}</p>
-        <p>Posted by {authorFirstName} {authorLastName}</p>
-        <p className="grey-text">26-JAN-2021, 9PM</p>
+        <p>
+          Posted by {authorFirstName} {authorLastName}
+        </p>
+        <p className="grey-text">{moment(createdAt.toDate()).calendar()}</p>
       </div>
     </div>
   );
