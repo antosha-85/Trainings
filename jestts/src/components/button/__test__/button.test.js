1747321ffe73159e6cd800996c6605d7998a4ee9
antosha-85 <antosha-85@yandex.ru>
2021-02-26 10:42:00 -0700

    Added the second test that checks that my button renders correсtly and contains the same label that I am using in props

--- jestts/src/components/button/__test__/button.test.js
+++ jestts/src/components/button/__test__/button.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "../Button";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"
 
-it('renders without crashing', ()=> {
-    const div = document.createElement("div");
-    ReactDOM.render(<Button/>, div)
-})
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Button />, div);
+});
+
+it("renders button correctly", () => {
+    const {getByTestId} = render(<Button label="click me"/>)
+    expect(getByTestId('button')).toHaveTextContent("click me")
+});
