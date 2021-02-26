e90e2850ceae915d53a2f86dd42255b1e8a5c5d8
antosha-85 <antosha-85@yandex.ru>
2021-02-25 17:28:11 -0700

    Created a new button.js file and a test folder plus button.test.js file and imported React, ReactDom and Button, no tests yet

--- jestts/src/App.js
+++ jestts/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Button from "./components/button/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Button label="click me please, bro!" />
+      </header>
     </div>
   );
 }
