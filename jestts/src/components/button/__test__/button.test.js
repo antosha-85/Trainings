409881484b8c57adfad076bf32efc57d07cb05f8
antosha-85 <antosha-85@yandex.ru>
2021-02-26 10:46:38 -0700

    Added cleanup function that I am using after doing each test that allows me to do very similar tests

--- jestts/src/components/button/__test__/button.test.js
+++ jestts/src/components/button/__test__/button.test.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "../Button";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
+afterEach(cleanup);
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Button />, div);
 });
 
 it("renders button correctly", () => {
-    const {getByTestId} = render(<Button label="click me"/>)
-    expect(getByTestId('button')).toHaveTextContent("click me")
+  const { getByTestId } = render(<Button label="click me" />);
+  expect(getByTestId("button")).toHaveTextContent("click me");
+});
+
+it("renders button correctly", () => {
+  const { getByTestId } = render(<Button label="save" />);
+  expect(getByTestId("button")).toHaveTextContent("save");
 });
