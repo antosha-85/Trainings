516905884586bf7e694b0aba286937fba1a5fd53
antosha-85 <antosha-85@yandex.ru>
2021-03-19 03:05:53 -0600

    Created an useForm custom hook to simulate the way I update my inputs

--- react-hooks-tr/src/App.js
+++ react-hooks-tr/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
+import { useForm } from "./useForm";
 
 function App() {
   const [count, setCount] = useState(10);
   const [count2, setCount2] = useState(20);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [values, setValues] = useForm({ email: "", password: "" });
+  // const [password, setPassword] = useState("");
 
   return (
     <div className="App">
@@ -19,8 +20,18 @@ function App() {
       </button>
       <div>count 1: {count}</div>
       <div>count 2: {count2}</div>
-      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <input
+        type="email"
+        name="email"
+        value={values.email}
+        onChange={setValues}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={setValues}
+      />
     </div>
   );
 }
