7c298832f475e0dfb1e603456e69befd3c92f6c3
antosha-85 <antosha-85@yandex.ru>
2021-03-19 02:50:52 -0600

    Created two inputs for email and password

--- react-hooks-tr/src/App.js
+++ react-hooks-tr/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(10);
   const [count2, setCount2] = useState(20);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="App">
@@ -17,6 +19,8 @@ function App() {
       </button>
       <div>count 1: {count}</div>
       <div>count 2: {count2}</div>
+      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
     </div>
   );
 }
