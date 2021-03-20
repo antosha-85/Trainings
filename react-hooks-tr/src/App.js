3fd806377db8c716f32226f7182da6947473a319
antosha-85 <antosha-85@yandex.ru>
2021-03-20 05:56:27 -0600

    Created new component Hello and render it conditionally, using useEffect hook to log the moment when the Hello component mounts and unmounts

--- react-hooks-tr/src/App.js
+++ react-hooks-tr/src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Hello } from "./Hello";
 import { useForm } from "./useForm";
 
 function App() {
   const [count, setCount] = useState(10);
   const [count2, setCount2] = useState(20);
-  const [values, setValues] = useForm({ email: "", password: "" });
+  const [values, setValues] = useForm({ email: "", password: "", firstName: "" });
   // const [password, setPassword] = useState("");
+  // useEffect(()=> {
+  //   console.log('render')
+  //   return ()=> {
+  //     console.log('unmount!')
+  //   }
+  // }, [])
+  const [showHello, setShowHello] = useState(true)
 
   return (
     <div className="App">
+      <button onClick={()=> setShowHello(!showHello)}>Toggle Hello!</button>
+      {showHello ? <Hello/> : null}
       <button
         onClick={() => {
           setCount((currentCount) => currentCount + 1);
@@ -25,12 +35,21 @@ function App() {
         name="email"
         value={values.email}
         onChange={setValues}
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        name="firstName"
+        value={values.firstName}
+        onChange={setValues}
+        placeholder="First Name"
       />
       <input
         type="password"
         name="password"
         value={values.password}
         onChange={setValues}
+        placeholder="Password"
       />
     </div>
   );
