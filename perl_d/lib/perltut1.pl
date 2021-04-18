4178e4ece6df37219ff5097b47fb8b6234831aa8
antosha-85 <antosha-85@yandex.ru>
2021-04-18 03:16:10 -0600

    refreshed all the basics of perl for hashes

--- perl_d/lib/perltut1.pl
+++ perl_d/lib/perltut1.pl
@@ -246,3 +246,38 @@ print join (", ", @odds_array), "\n";
 
 my @dbl_array = map {$_ * 2} @number_array;
 print join (", ", @dbl_array), "\n";
+
+my %employees = (
+    "Sue"=>35,
+    "Paul"=> 43,
+    "Sam"=>39
+);
+
+printf("Sue is %d \n", $employees{Sue});
+
+$employees{Frank} = 44;
+
+while (my ($k, $v) = each %employees) {
+    print "$k $v\n"
+}
+
+my @ages = @employees{"Sue", "Sam"};
+say @ages;
+
+my @hash_array = %employees;
+say %employees;
+
+delete $employees{'Frank'};
+
+while (my ($k, $v) = each %employees) {
+    print "$k $v \n"
+}
+
+say ((exists $employees{'Sam'}) ? 'Sam is here' : 'No Sam');
+
+for my $key (keys %employees) {
+    if ($employees{$key} == 35) {
+        say "Hi Sue";
+    }
+}
+
