38505412e631c3918a4e9f32ea7ba41daae34e56
antosha-85 <antosha-85@yandex.ru>
2021-04-18 03:49:52 -0600

    refreshed all the basics of perl for subroutines

--- perl_d/lib/perltut1.pl
+++ perl_d/lib/perltut1.pl
@@ -281,3 +281,72 @@ for my $key (keys %employees) {
     }
 }
 
+sub get_random {
+    return int(rand 11);
+}
+
+say "Random Number ", get_random();
+
+sub getrandom_max {
+    my ($max_num) = @_;
+    $max_num ||= 11;
+    return int(rand $max_num)
+}
+
+say "Random Number ", getrandom_max(100);
+
+sub get_sum {
+    my ($num_1, $num_2) = @_;
+    $num_1 ||= 1;
+    $num_2 ||= 2;
+    return $num_1 + $num_2;
+}
+
+say "Sum for 1 and 3 is ", get_sum(1, 3);
+
+sub sum_many {
+    my $sum = 0;
+    foreach my $val(@_) {
+        $sum += $val;
+    }
+    return $sum;
+}
+
+say "Sum : ", sum_many(1, 2, 3, 4, 5);
+
+sub increment {
+    state $execute_total = 0;
+    $execute_total++;
+    say "Executed $execute_total times!";
+}
+
+increment();
+increment();
+
+sub double_array {
+    my @num_array = @_;
+    $_ *= 2 for @num_array;
+    return @num_array;
+}
+
+my @rand_array = (1, 2, 3, 4, 5);
+
+print join (", ", double_array(@rand_array)), "\n";
+
+sub get_mults {
+    my ($rand_num) = @_;
+    $rand_num ||= 1;
+    return $rand_num * 2, $rand_num * 3;
+}
+
+my ($dbl_num, $trip_num) = get_mults(3);
+say "$dbl_num $trip_num";
+
+sub factrorial {
+    my ($num) = @_;
+    return 0 if $num <= 0;
+    return 1 if $num == 1;
+    return $num * factrorial($num - 1);
+}
+
+say 'Factorial 4 = ', factrorial(4);
